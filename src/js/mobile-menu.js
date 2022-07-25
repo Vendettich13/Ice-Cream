9f5a3bac059643831b255e88c8abbd3ba3bbf527
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2022-07-25 18:00:51 +0300

    Update mob menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,5 +1,6 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
+    const closeModalBtn = document.querySelector("[data-menu-button-close]");
     const mobileMenuRef = document.querySelector("[data-menu]");
 
     menuBtnRef.addEventListener('click', () => {
@@ -10,6 +11,17 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenuRef.classList.toggle("is-open");
-        document.body.classList.toggle("menu-open")
+        document.body.classList.toggle("no-scroll")
+    });
+
+    closeModalBtn.addEventListener('click' , () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll")
     });
 })();
