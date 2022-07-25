9cc48245ce563c05865562203c6cfd9fc48ffc9f
Vendettich13 <104316245+Vendettich13@users.noreply.github.com>
2022-07-25 15:35:24 +0000

    Deploying to gh-pages from @ Vendettich13/web-hunters-project@bafb70f77f6520586be2a3b4b305756cad93f1fc 🚀

--- index.825cd4f9.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),document.body.classList.toggle("menu-open")}))}();
-//# sourceMappingURL=index.825cd4f9.js.map
