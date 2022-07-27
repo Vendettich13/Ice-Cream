e79803589055e87188f1ee7ad01c766bb75a3505
Victor Litvynchuk <vendettich13@icloud.com>
2022-07-28 01:17:23 +0300

    update/js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -37,4 +37,13 @@
         refs.modal.classList.toggle('is-open');
     }
 
+    $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
+
 })();
--- src/js/toup.js
+++ src/js/toup.js
@@ -1,10 +1,18 @@
 window.addEventListener('scroll', function () {
     var scroll = document.querySelector('.up');
-    scroll.classList.toggle("activity", window.scrollY > 800)
+    scroll.classList.toggle("activity", window.scrollY > 500)
 })
 function scrollTopTop() {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
      })
- }
+}
+   $(document).ready(function(){
+    $("#toup").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
