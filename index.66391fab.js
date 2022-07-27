290dbfe62cb3b56e672eb7255f0378a5092ef5da
Vendettich13 <104316245+Vendettich13@users.noreply.github.com>
2022-07-27 21:15:54 +0000

    Deploying to gh-pages from @ Vendettich13/web-hunters-project@0c5830eeec782d319c03ea10aca613a60e90604a 🚀

---
+++ index.66391fab.js
@@ -0,0 +1,2 @@
+function scrollTopTop(){window.scrollTo({top:0,behavior:"smooth"})}window.addEventListener("scroll",(function(){document.querySelector(".up").classList.toggle("activity",window.scrollY>800)}));
+//# sourceMappingURL=index.66391fab.js.map
